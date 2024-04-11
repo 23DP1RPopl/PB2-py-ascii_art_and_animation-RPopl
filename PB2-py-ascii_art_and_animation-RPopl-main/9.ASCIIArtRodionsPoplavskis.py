c5b99b08865f5843a4a0c125c8c42f608def64ea
23DP1RPopl <A230851RP@rvt.lv>
2024-04-11 14:11:25 +0300

    .

---
+++ PB2-py-ascii_art_and_animation-RPopl-main/9.ASCIIArtRodionsPoplavskis.py
@@ -0,0 +1,52 @@
+from time import sleep
+from os import system
+
+
+
+system("cls")
+print("R")
+print(" O")
+print("  T")
+sleep(0.5)
+
+system("cls")
+print(" R")
+print(" O")
+print(" T")
+sleep(0.5)
+
+system("cls")
+print("  R")
+print(" O")
+print("T")
+sleep(0.5)
+
+system("cls")
+print("")
+print("TOR")
+print("")
+sleep(0.5)
+
+system("cls")
+print("T")
+print(" O")
+print("  R")
+sleep(0.5)
+
+system("cls")
+print(" T")
+print(" O")
+print(" R")
+sleep(0.5)
+
+system("cls")
+print("  T")
+print(" O")
+print("R")
+sleep(0.5)
+
+system("cls")
+print("")
+print("ROT")
+print("")
+sleep(0.5)
